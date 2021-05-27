2c9651a0ac261280002caf7d7182c80c05abbcd3
Ruizhi Zhong <ruizhi.zhong@outlook.com>
2021-05-27 21:29:52 +0800

    bugfix

--- BestTimeToBuyAndSellStockIV.java
+++ BestTimeToBuyAndSellStockIV.java
@@ -24,7 +24,7 @@ public class BestTimeToBuyAndSellStockIV {
 
         for(int i = 1; i < prices.length; i++) {
             int diff = prices[i] - prices[i - 1];
-            for(int j = 1; j <= k; k++) {
+            for(int j = 1; j <= k; j++) {
                 local[i][j] = Math.max(global[i - 1][j - 1] + Math.max(diff, 0), local[i - 1][j] + diff);
                 global[i][j] = Math.max(local[i][j], global[i - 1][j]);
             }
