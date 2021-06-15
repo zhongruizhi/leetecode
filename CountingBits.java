b22d1c581add2040106b2de3c2a328cff8e7d9ab
Ruizhi Zhong <ruizhi.zhong@outlook.com>
2021-06-15 16:27:31 +0800

    338. Counting Bits DP

--- CountingBits.java
+++ CountingBits.java
@@ -5,6 +5,12 @@
 import java.util.Arrays;
 
 public class CountingBits {
+    /**
+     * Brute Force
+     * 
+     * @param n
+     * @return
+     */
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
         for (int i = 0; i <= n; i++) {
@@ -19,8 +25,24 @@ public class CountingBits {
         return ans;
     }
 
+    /**
+     * Dynamic Programming
+     * 
+     * @param n
+     * @return
+     */
+    public int[] countBitsDp(int n) {
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i / 2] + i % 2;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         CountingBits solution = new CountingBits();
         System.out.println(Arrays.toString(solution.countBits(5)));
+        System.out.println(Arrays.toString(solution.countBitsDp(5))); 
     }
 }
